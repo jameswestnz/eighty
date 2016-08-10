88b6e7ed58a172795940125178e63c65716e07eb
James West <james@jameswest.co.nz>
2016-08-11 10:10:15 +1200

    Moved to hapi promises

--- bin/start.js
+++ bin/start.js
@@ -3,11 +3,11 @@ var
   cli     = require('../cli')
 ;
 
-cli(function (error, server) {
-  if (error) {
-    throw error
-  }
-
-  console.log('Eighty has started on:', server.info.uri)
-  console.log('Stop eighty with control + c')
-})
+cli()
+  .then(function(server) {
+    console.log('Eighty has started on:', server.info.uri)
+    console.log('Stop eighty with control + c')
+  })
+  .catch(function(err){
+    console.log('Eighty server failed to start: ' + err)
+  })
--- cli/index.js
+++ cli/index.js
@@ -5,46 +5,45 @@ var
   Hapi            = require('hapi'),
   h2o2            = require('h2o2'),
   inert           = require('inert'),
+  Promise         = require('promise'),
   rc              = require('rc'),
 
   // local
   eighty          = require('../server')
 ;
 
-function cli(callback) {
+function cli() {
   var options = rc('eighty', {
-    port: 8080
+    port: 9292
   });
 
   var server = new Hapi.Server();
-  server.connection({
-    port: options.port
-  });
 
-  server.register([
-    {
-      register: h2o2
-    },
-    {
-      register: inert
-    },
-    {
+  server.connection({port: options.port})
+
+  // register dependencies
+  return server.register([h2o2, inert])
+    .then(server.register.bind(server, {
       register: eighty,
       options: options
-    }
-  ], function (error) {
-    if (error) {
-      return callback(error)
-    }
+    }))
 
-    var eighty = server.plugins['eighty']['api'];
+    .then(function () {
+      var eighty = server.plugins.eighty.api;
 
-    eighty.on('app:connect', function(name, options){
-      console.log('App added: ' + name);
-    });
+      eighty.on('app:connect', function(name, options){
+        console.log('App added: ' + name);
+      });
 
-    server.start(function (error) {
-      callback(error, server);
-    });
-  })
+      return server.start()
+        .then(function(){
+          return server;
+        })
+      ;
+    })
+
+    .catch(function(err){
+      console.log(err);
+      return Promise.reject(err)
+    })
 }
